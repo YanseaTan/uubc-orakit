184552efe88f2ef8ee230ea695320290b05afb0d
Pengwei.Zhang <vilaseaka@outlook.com>
2022-02-06 18:27:47 +0800

    update the OriKit basic framework

--- sprint1/pwz/core/NLPChunk.java
+++ sprint1/pwz/core/NLPChunk.java
@@ -1,12 +1,26 @@
 package pwz.core;
 
+import pwz.core.ServiceToken;
+
+import java.util.ArrayList;
+
 public class NLPChunk {
-    public static void seyHello(){
+    public static void sayHello(){
         System.out.println("Hi~ what can I do for you?");
     }
 
-    public static String tokenAnalyze(String sentence){
-        if(sentence.contains("snake"))return "snake";
-        else return "hello";
+    public static void say404(){
+        System.out.println("Sorry, I don't understand.");
+    }
+
+    public static ArrayList<String> tokenAnalyze(String sentence){
+        ArrayList<String> tokens = new ArrayList<>();
+        for (String token: ServiceToken.tokenInfo.keySet()){
+            if (sentence.contains(token)){
+                tokens.add(token);
+            }
+        }
+        if (tokens.size() == 0) say404();
+        return tokens;
     }
 }
